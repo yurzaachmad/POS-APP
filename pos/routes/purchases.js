e05f88abcf1840cbfbc1cbef2e1560b29a1d73f7
yurza <yurzaachmad@gmail.com>
2023-07-01 00:12:36 +0700

    create fitur bread purchase

--- pos/routes/purchases.js
+++ pos/routes/purchases.js
@@ -4,14 +4,41 @@ const isLoggedIn = require("../helpers/util");
 var moment = require("moment");
 
 module.exports = function (db) {
+  router.get("/datapurchase", async (req, res) => {
+    let params = [];
+
+    if (req.query.search.value) {
+      params.push(`invoice ilike '%${req.query.search.value}%'`);
+    }
+
+    const limit = req.query.length;
+    const offset = req.query.start;
+    const sortBy = req.query.columns[req.query.order[0].column].data;
+    const sortMode = req.query.order[0].dir;
+
+    const total = await db.query(
+      `select count(*) as total from purchases${
+        params.length > 0 ? ` where ${params.join(" or ")}` : ""
+      }`
+    );
+    const data = await db.query(
+      `select * from purchases${
+        params.length > 0 ? ` where ${params.join(" or ")}` : ""
+      } order by ${sortBy} ${sortMode} limit ${limit} offset ${offset} `
+    );
+    const response = {
+      draw: Number(req.query.draw),
+      recordsTotal: total.rows[0].total,
+      recordsFiltered: total.rows[0].total,
+      data: data.rows,
+    };
+    res.json(response);
+  });
   router.get("/purchases", function (req, res, next) {
-    console.log(req.session.user);
-    // console.log(userid);
     db.query("select * from purchases", (err, data) => {
       if (err) {
         console.log(err);
       }
-      // console.log("ini", data.rows);
       res.render("purchases/purchase", {
         data: data.rows,
       });
@@ -28,9 +55,6 @@ module.exports = function (db) {
         if (err) {
           console.log(err);
         }
-
-        // console.log("Data baru yang dimasukkan:", data.rows[0]);
-
         res.redirect(`/purchase/${data.rows[0].invoice}`);
       }
     );
@@ -71,7 +95,6 @@ module.exports = function (db) {
       "select * from goods where barcode = $1",
       [barcode],
       (err, item) => {
-        // console.log(item);
         if (err) {
           console.log(err);
         }
@@ -101,9 +124,115 @@ module.exports = function (db) {
           console.log(err);
         }
         console.log(item, "ini");
-        res.redirect("/purchases/add");
+        res
+          .status(200)
+          .json({ message: "Data berhasil dimasukkan ke database." });
       }
     );
+  });
+
+  router.post("/purchase/add/purchases", (req, res) => {
+    const invoice = req.body.invoice;
+    const formattedValue = req.body.totalsum.replace(/[^0-9.,-]/g, ""); // Menghapus karakter selain angka, koma, dan tanda minus
+    const totalsum = parseFloat(formattedValue.replace(",", "")); // Menghapus koma sebagai pemisah ribuan
+
+    const { supply } = req.body;
+    const { userid } = req.session.user;
+    db.query(
+      "update purchases set totalsum = $1, supplier = $2, operator = $3 WHERE invoice = $4",
+      [totalsum, supply, userid, invoice],
+      (err, item) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(item, "iniii");
+        res.redirect("/purchases");
+      }
+    );
+  });
+
+  router.get("/purchase/edit/:invoice", (req, res) => {
+    const { invoice } = req.params;
+    const { userid } = req.session.user;
+    db.query(
+      "select * from purchases where invoice = $1",
+      [invoice],
+      (err, item) => {
+        console.log("this", item);
+        db.query(
+          "select * from users where userid = $1",
+          [userid],
+          (err, items) => {
+            db.query("select * from goods", (err, datagoods) => {
+              db.query("select * from suppliers", (err, supply) => {
+                db.query("select * from purchaseitems", (err, itempurchase) => {
+                  console.log(itempurchase.rows[0], "purchaseitems");
+                  res.render("purchases/purchaseform", {
+                    data: item.rows[0],
+                    dataa: items.rows[0],
+                    moment,
+                    datagood: datagoods.rows,
+                    suppliers: supply.rows,
+                    itemspurchase: itempurchase.rows[0],
+                  });
+                });
+              });
+            });
+          }
+        );
+      }
+    );
+  });
+
+  router.get("/purchase/get/edit/item/:invoice", (req, res) => {
+    const { invoice } = req.params;
+    console.log(invoice);
+    db.query(
+      "SELECT purchaseitems.*, goods.name FROM purchaseitems LEFT JOIN goods ON purchaseitems.itemcode = goods.barcode WHERE purchaseitems.invoice = $1 ORDER BY purchaseitems.id",
+      [invoice],
+      (err, itempurchase) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log("ini", itempurchase);
+        res.json(itempurchase.rows);
+      }
+    );
+  });
+
+  router.get("/purchases/deleteitems/:id", (req, res) => {
+    const { id } = req.params;
+    db.query(
+      "SELECT invoice FROM purchaseitems WHERE id = $1",
+      [id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+
+        const invoice = result.rows[0].invoice;
+        db.query(
+          "delete from purchaseitems where id = $1",
+          [id],
+          (err, itempurchase) => {
+            if (err) {
+              console.log(err);
+            }
+            res.redirect(`/purchase/edit/${invoice}`);
+          }
+        );
+      }
+    );
+  });
+
+  router.get("/purchase/delete/:id", (req, res) => {
+    const id = req.params.id;
+    db.query("delete from purchases where invoice = $1", [id], (err) => {
+      if (err) {
+        console.log("hapus data suppliers gagal");
+      }
+      res.redirect("/purchases");
+    });
   });
 
   return router;
